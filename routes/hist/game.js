30d9e01d55e69d4ede18710f5f39171c43dca2bd
tony <137@tuta.io>
2017-05-29 20:16:06 +0800

    Add routes for coin performance history
    AMP 	Synereo AMP
    ARDR 	Ardor
    BCN 	Bytecoin
    BCY 	BitCrystals
    BELA 	Belacoin
    BLK 	BlackCoin
    BTC 	Bitcoin
    BTCD 	BitcoinDark
    BTM 	Bitmark
    BTS 	BitShares
    BURST 	Burst
    CLAM 	CLAMS
    DASH 	Dash
    DCR 	Decred
    DGB 	DigiByte
    DOGE 	Dogecoin
    EMC2 	Einsteinium
    ETC 	Ethereum Classic
    ETH 	Ethereum
    EXP 	Expanse
    FCT 	Factom
    FLDC 	FoldingCoin
    FLO 	Florincoin
    GAME 	GameCredits
    GNO 	Gnosis
    GNT 	Golem
    GRC 	Gridcoin Research
    HUC 	Huntercoin
    LBC 	LBRY Credits
    LSK 	Lisk
    LTC 	Litecoin
    MAID 	MaidSafeCoin
    NAUT 	Nautiluscoin
    NAV 	NAVCoin
    NEOS 	Neoscoin
    NMC 	Namecoin
    NOTE 	DNotes
    NXC 	Nexium
    NXT 	NXT
    OMNI 	Omni
    PASC 	PascalCoin
    PINK 	Pinkcoin
    POT 	PotCoin
    PPC 	Peercoin
    RADS 	Radium
    REP 	Augur
    RIC 	Riecoin
    SBD 	Steem Dollars
    SC 	Siacoin
    SJCX 	Storjcoin X
    STEEM 	STEEM
    STR 	Stellar
    STRAT 	Stratis
    SYS 	Syscoin
    VIA 	Viacoin
    VRC 	VeriCoin
    VTC 	Vertcoin
    XBC 	BitcoinPlus
    XCP 	Counterparty
    XEM 	NEM
    XMR 	Monero 	w
    XPM 	Primecoin
    XRP 	Ripple
    XVC 	Vcash
    ZEC 	Zcash

---
+++ routes/hist/game.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+// setup node-rest-client
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
+/* This route receives HTTP GET request from client to return historical data on coin performance,
+ * then makes HTTP GET request to CoinCap API and responds to client with received response
+ * (JSON Object in the following format: {"market_cap":[*Array of Arrays (chart data)*],
+ *                                        "price":[*Array of Arrays (chart data)*]} )            */
+
+// listen for request
+router.get('/', function(req, res) {
+    console.log('received user request');
+
+    // make HTTP GET request to external API
+    client.get("http://www.coincap.io/history/GAME", function (data) {
+        // parsed response body as js object
+        console.log('sending data:  ' + data);
+
+        // send received object to client
+        res.send(data);
+        console.log('success!');
+    });
+});
+
+module.exports = router;
