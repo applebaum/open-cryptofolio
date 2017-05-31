fe9074cd0294d37e74723289873c5fa308d4bbb0
tony <137@tuta.io>
2017-05-31 15:02:34 +0800

    Clean up server.js, add comments

--- server.js
+++ server.js
@@ -5,6 +5,33 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var app = express();
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+// setup node-rest-client dependeices
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
+// setup socket.io dependencies
+var io = require('./io');
+import ioClient from 'socket.io-client';
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//setup routes for historical coin data
 var routes = require('./routes/index');
 var AMP = require('./routes/hist/amp');
 var ARDR = require('./routes/hist/ardr');
@@ -71,33 +98,6 @@ var XRP = require('./routes/hist/xrp');
 var XVC = require('./routes/hist/xvc');
 var ZEC = require('./routes/hist/zec');
 
-var app = express();
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-
-// setup node-rest-client
-var Client = require('node-rest-client').Client;
-var client = new Client();
-
-// setup socket.io
-var io = require('./io');
-import ioClient from 'socket.io-client';
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
-
-// uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-//setup routes for historical data
 app.use('/', routes);
 app.use('/hist/amp', AMP);
 app.use('/hist/ardr', ARDR);
@@ -164,6 +164,8 @@ app.use('/hist/xrp', XRP);
 app.use('/hist/xvc', XVC);
 app.use('/hist/zec', ZEC);
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -195,129 +197,15 @@ app.use(function(err, req, res, next) {
     });
 });
 
-//////////// Socket.io - FETCH DATA FROM EXTERNAL SOCKET \\\\\\\\\\\\\\\\\\\
-// var quote = {};
-//
-// // subscribe to external socket
-// var socket = ioClient.connect('https://streamer.cryptocompare.com');
-//
+/*Socket.io connects to external CoinCap socket API and fetches historical data on all available coins,
+* it then emits the data to client as soon as it recieves it*/
 
-    let subscription = [
-        '5~CCCAGG~XMR~BTC',
-        '5~CCCAGG~BCN~BTC',
-        '5~CCCAGG~AMP~BTC',
-        '5~CCCAGG~ARDR~BTC',
-        '5~CCCAGG~BCY~BTC',
-        '5~CCCAGG~BELA~BTC',
-        '5~CCCAGG~BLK~BTC',
-        '5~CCCAGG~BTM~BTC',
-        '5~CCCAGG~BTS~BTC',
-        '5~CCCAGG~BURST~BTC',
-        '5~CCCAGG~CLAM~BTC',
-        '5~CCCAGG~DASH~BTC',
-        '5~CCCAGG~DCR~BTC',
-        '5~CCCAGG~DGB~BTC',
-        '5~CCCAGG~DOGE~BTC',
-        '5~CCCAGG~EMC2~BTC',
-        '5~CCCAGG~ETC~BTC',
-        '5~CCCAGG~ETH~BTC',
-        '5~CCCAGG~EXP~BTC',
-        '5~CCCAGG~FCT~BTC',
-        '5~CCCAGG~FLDC~BTC',
-        '5~CCCAGG~FLO~BTC',
-        '5~CCCAGG~GAME~BTC',
-        '5~CCCAGG~GNO~BTC',
-        '5~CCCAGG~GNT~BTC',
-        '5~CCCAGG~GRC~BTC',
-        '5~CCCAGG~HUC~BTC',
-        '5~CCCAGG~LBC~BTC',
-        '5~CCCAGG~LSK~BTC',
-        '5~CCCAGG~LTC~BTC',
-        '5~CCCAGG~MAID~BTC',
-        '5~CCCAGG~NAUT~BTC',
-        '5~CCCAGG~NAV~BTC',
-        '5~CCCAGG~NEOS~BTC',
-        '5~CCCAGG~NMC~BTC',
-        '5~CCCAGG~NOTE~BTC',
-        '5~CCCAGG~NXC~BTC',
-        '5~CCCAGG~NXT~BTC',
-        '5~CCCAGG~OMNI~BTC',
-        '5~CCCAGG~PASC~BTC',
-        '5~CCCAGG~PINK~BTC',
-        '5~CCCAGG~POT~BTC',
-        '5~CCCAGG~PPC~BTC',
-        '5~CCCAGG~RADS~BTC',
-        '5~CCCAGG~REP~BTC',
-        '5~CCCAGG~RIC~BTC',
-        '5~CCCAGG~SBD~BTC',
-        '5~CCCAGG~SC~BTC',
-        '5~CCCAGG~SJCX~BTC',
-        '5~CCCAGG~STEEM~BTC',
-        '5~CCCAGG~XLM~BTC',
-        '5~CCCAGG~STRAT~BTC',
-        '5~CCCAGG~SYS~BTC',
-        '5~CCCAGG~VIA~BTC',
-        '5~CCCAGG~VRC~BTC',
-        '5~CCCAGG~VTC~BTC',
-        '5~CCCAGG~XBC~BTC',
-        '5~CCCAGG~XCP~BTC',
-        '5~CCCAGG~XEM~BTC',
-        '5~CCCAGG~XPM~BTC',
-        '5~CCCAGG~XRP~BTC',
-        '5~CCCAGG~XVC~BTC',
-        '5~CCCAGG~ZEC~BTC'];
+//connect to external socket
+let socket = ioClient.connect('http://socket.coincap.io');
 
-    // let fetchData = () => {
-    //
-    //     socket.emit('SubAdd', {subs: subscription});
-    // };
-
-
-//receive data
-
-// CRYPTOCOMPARE
-//         socket.on("m", function (message) {
-//             let messageType = message.substring(0, message.indexOf("~"));
-//             let res = {};
-//             if (messageType === CCC.STATIC.TYPE.CURRENTAGG) {
-//                 res = CCC.CURRENT.unpack(message);
-//                 console.log(res);
-//                 updateQuote(res);
-//             }
-//         });
-//
-//         // assign keys, send to client
-//         let updateQuote = (result) => {
-//
-//             let keys = Object.keys(result);
-//
-//             for (let i = 0; i < keys.length; ++i) {
-//                 quote[keys[i]] = result[keys[i]];
-//             }
-//             io.emit('m', quote);
-//             // console.log(quote);
-//         };
-//
-// setTimeout(fetchData, 100);
-//
-// setInterval(fetchData, 180000);
-
-//Format: {SubscriptionId}~{ExchangeName}~{FromSymbol}~{ToSymbol}
-//Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
-//For aggregate quote updates use CCCAGG as market
-
-// COINCAP
-
-var socket = ioClient.connect('http://socket.coincap.io');
-
+//when received data, emit it ot client
 socket.on('trades', function (tradeMsg) {
     io.emit('trades', tradeMsg);
-
-    // console.log(tradeMsg.message);
 });
-
-// socket.on('global', function (globalMsg) {
-//     console.log(globalMsg);
-// });
 
 module.exports = app;
